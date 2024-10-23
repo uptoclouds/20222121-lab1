66f3b0a684bb6da898177c6ef2c60f48bbb24845
fsr <14387260+fsr144@user.noreply.gitee.com>
2024-10-23 15:45:48 +0800

    10.6

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -13,6 +13,7 @@ const productDisplay = {
                 <p v-if="inventory > 10">In Stock</p>
                 <p v-else-if="inventory <= 10 && inventory > 0">Almost out of Stock</p>
                 <p v-else>Out of Stock</p>
+                <p>Shipping: {{shipping}}</p>
                 <ul>
                     <li v-for="detail in details">{{detail}}</li>
                 </ul>
@@ -24,11 +25,20 @@ const productDisplay = {
             </div>
         </div>
     `,
-    setup() {
+    props: {
+        premium: Boolean
+    },
+    setup(props, { emit }) {
+        const shipping = computed(()=>{
+            if (props.premium){
+                return 'Free'
+            } else {
+                return 30
+            }
+           
+        })
         const product = ref('Boots')
         const brand = ref('SE 331')
-        // const image = ref('./assets/images/socks_green.jpg')
-        // const inStock = ref(true)
         const inventory = ref(100)
         const details = ref([
             '50% cotton',
@@ -37,7 +47,7 @@ const productDisplay = {
         ])
         const variants = ref([
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
         ])
         const selectedVariant = ref(0)
         const cart = ref(0)
@@ -51,13 +61,13 @@ const productDisplay = {
             return variants.value[selectedVariant.value].quantity
         })
         function addToCart() {
-            cart.value += 1
+            emit('add-to-cart', variants.value[selectedVariant.value].id)
         }
         const title = computed(() => {
             return brand.value + ' ' + product.value
         })
         function updateImage(variantImage) {
-            image.value = variantImage
+            image.value = variantImage;
         }
         return {
             title,
@@ -68,7 +78,8 @@ const productDisplay = {
             variants,
             addToCart,
             updateImage,
-            updateVariant
+            updateVariant,
+            shipping
         }
     }
 }
